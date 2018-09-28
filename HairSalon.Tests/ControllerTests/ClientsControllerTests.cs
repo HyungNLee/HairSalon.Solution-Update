4e192632a571547087dcbc75a1c2f15c7f5695f9
Hyung Lee <hyungnaelee@gmail.com>
2018-09-28 12:55:11 -0700

    Add controller testing.

---
+++ HairSalon.Tests/ControllerTests/ClientsControllerTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class ClientsControllerTest
+  {
+    [TestMethod]
+    public void Details_ReturnsCorrectView_True()
+    {
+      //Arrange
+      Stylist newStylist = new Stylist("Ryan");
+      newStylist.Save();
+      Client newClient = new Client("Ryan", 1);
+      newClient.Save();
+      ClientsController controller = new ClientsController();
+
+      //Act
+      ActionResult detailsView = controller.Details(1);
+
+      //Assert
+      Assert.IsInstanceOfType(detailsView, typeof(ViewResult));
+    }
+
+    [TestMethod]
+    public void Details_HasCorrectModelType_ItemList()
+    {
+      //Arrange
+      Stylist newStylist = new Stylist("Ryan");
+      newStylist.Save();
+      Client newClient = new Client("Ryan", 1);
+      newClient.Save();
+      ClientsController controller = new ClientsController();
+      ViewResult detailsView = controller.Details(1) as ViewResult;
+
+      //Act
+      var result = detailsView.ViewData.Model;
+
+      //Assert
+      Assert.IsInstanceOfType(result, typeof(Client));
+    }
+  }
+}
